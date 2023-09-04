de7b6dcf79d6afbcbdf69d66a4df51f705838698
riyasaxena09 <135497287+riyasaxena09@users.noreply.github.com>
2023-09-04 16:32:42 +0530

    add

--- Contact.js
+++ Contact.js
@@ -1,14 +1,52 @@
 import { useNavigate } from "react-router-dom";
-import { useRef } from "react";
+import { useState} from "react";
+
 function Contact(){
+    const [mail,setemail]=useState();
+    const [urling,seturl]=useState();
+    
+
+    fetch('https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyBr8QUNIcET_N7fAjDUsO-MOeRg0Ntc8zc',{
+        method:'POST',
+        body:JSON.stringify({
+            idToken:localStorage.getItem("token"),
+        }),
+        headers:{
+            'Content-type':'aplication/json',
+        },
+    }).then((res)=>{
+        if(res.ok){
+return res.json();
+   }else{
+            return res.json().then((data)=>{
+                let errormsg="need to update";
+               // if(data && data.error && data.error.message){
+                 //   errormsg=data.error.message;
+               // }
+               throw new Error(errormsg);
+            });
+        }
+    }).then((data)=>{
+        console.log(data.users[0].displayName)
+        console.log(data.users[0].photoUrl)
+  
+    })
+        function emailhandler(e){
+            setTimeout(()=>{
+                setemail(e.target.value);
+            },2000)
+        }
+        
+                function urlhandler(e){
+                    setTimeout(()=>{
+                    seturl(e.target.value); 
+                },2000)
+                }
+
     const nav=useNavigate();
-    const email=useRef();
-    const url=useRef();
-    function submit(e){
+function submit(e){
 e.preventDefault();
-const mail=email.current.value;
-const urling=url.current.value;
-console.log(mail,urling)
+
 let obj={
     idToken:localStorage.getItem("token"),
     displayName:mail,
@@ -52,9 +90,9 @@ nav('/');
         <form onSubmit={submit}>
             <h1>Contact Details</h1>
             <label>FullName:</label>      
-            <input ref={email}></input>
+            <input onChange={emailhandler}></input>
             <label>Photourl:</label>      
-            <input ref={url}></input>
+            <input onChange={urlhandler}></input>
             <button>Update</button>
            
             </form>
